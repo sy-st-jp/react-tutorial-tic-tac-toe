10e3eed67e1a2c77b646b95c282582b05ef4617f
Shun Yoshida <shun.yoshida.p@gmail.com>
2025-02-24 17:02:09 +0900

    refactor: replace to Square

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -1,39 +1,22 @@
 import type { FC } from "react";
+import {Square} from "../Square/Square.tsx";
 
 export const Board: FC = () => (
 	<>
 		<div className="board-row">
-			<button className="square" type="button">
-				1
-			</button>
-			<button className="square" type="button">
-				2
-			</button>
-			<button className="square" type="button">
-				3
-			</button>
+			<Square/>
+			<Square/>
+			<Square/>
 		</div>
 		<div className="board-row">
-			<button className="square" type="button">
-				4
-			</button>
-			<button className="square" type="button">
-				5
-			</button>
-			<button className="square" type="button">
-				6
-			</button>
+			<Square/>
+			<Square/>
+			<Square/>
 		</div>
 		<div className="board-row">
-			<button className="square" type="button">
-				7
-			</button>
-			<button className="square" type="button">
-				8
-			</button>
-			<button className="square" type="button">
-				9
-			</button>
+			<Square/>
+			<Square/>
+			<Square/>
 		</div>
 	</>
 );

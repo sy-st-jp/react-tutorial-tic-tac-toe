16de6d2ab3d5c276f164612c634906764d19487a
Shun Yoshida <shun.yoshida.p@gmail.com>
2025-02-24 16:49:23 +0900

    feat: create a board

--- src/components/Square/index.tsx
+++ src/components/Square/index.tsx
@@ -1,7 +1,39 @@
 import type { FC } from "react";
 
 export const Square: FC = () => (
-	<button className="square" type="button">
-		X
-	</button>
+	<>
+		<div className="board-row">
+			<button className="square" type="button">
+				1
+			</button>
+			<button className="square" type="button">
+				2
+			</button>
+			<button className="square" type="button">
+				3
+			</button>
+		</div>
+		<div className="board-row">
+			<button className="square" type="button">
+				4
+			</button>
+			<button className="square" type="button">
+				5
+			</button>
+			<button className="square" type="button">
+				6
+			</button>
+		</div>
+		<div className="board-row">
+			<button className="square" type="button">
+				7
+			</button>
+			<button className="square" type="button">
+				8
+			</button>
+			<button className="square" type="button">
+				9
+			</button>
+		</div>
+	</>
 );

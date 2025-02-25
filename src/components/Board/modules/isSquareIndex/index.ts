031414237783ff43c07b52efe41925bbe72dfecd
Shun Yoshida <shun.yoshida.p@gmail.com>
2025-02-25 09:11:56 +0900

    feat: implement module isSquareIndex

---
+++ src/components/Board/modules/isSquareIndex/index.ts
@@ -0,0 +1,5 @@
+import type { SquareIndex } from "../../../../types/SquareIndex";
+
+export const isSquareIndex = (index: number): index is SquareIndex => {
+	return index >= 0 && index < 9 && Number.isInteger(index);
+};

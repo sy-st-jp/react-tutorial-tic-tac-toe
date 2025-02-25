c6c241d57a306f12c92969b077292cc52fd2d2e1
Shun Yoshida <shun.yoshida.p@gmail.com>
2025-02-25 09:27:57 +0900

    refactor: use EmptyFunction

--- src/components/Game/components/Board/components/Square/index.tsx
+++ src/components/Game/components/Board/components/Square/index.tsx
@@ -1,9 +1,10 @@
 import type { FC } from "react";
+import type { EmptyFunction } from "../../../../../../types/EmptyFunction";
 import type { SquareValue } from "../../../../../../types/SquareValue";
 
 type Props = {
 	value: SquareValue;
-	onClick: () => void;
+	onClick: EmptyFunction;
 };
 
 export const Square: FC<Props> = (props) => {
